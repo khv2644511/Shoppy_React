e8e186a5e494ca84aaa044768f6adbae968ec9fd
khv2644511 <khv2644511@naver.com>
2022-11-27 00:26:38 +0900

    setting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 }
+
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,42 @@
-import React from "react";
-// import ReactDOM from 'react-dom'; //구버전
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import ProductDetail from './pages/ProductDetail';
+import NewProduct from './pages/NewProduct';
+import MyCart from './pages/MyCart';
+import NotFound from './pages/NotFound';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products', element: <AllProducts /> },
+      {
+        path: '/products/new',
+        element: <NewProduct />,
+      },
+      {
+        path: '/products/:id',
+        element: <ProductDetail />,
+      },
+      {
+        path: '/carts',
+        element: <MyCart />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
