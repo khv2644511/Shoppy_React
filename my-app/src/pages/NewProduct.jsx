f13f2c7bb2a584a59d3a22e0ce5ed3e0332b699c
khv2644511 <khv2644511@naver.com>
2023-03-24 02:06:10 +0900

    Feat: 제품 등록중, 등록완료 UI 추가

--- my-app/src/pages/NewProduct.jsx
+++ my-app/src/pages/NewProduct.jsx
@@ -6,6 +6,8 @@ import Button from "../components/ui/Button";
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
+  const [isUploading, setIsUploading] = useState(false);
+  const [success, setSuccess] = useState();
 
   const handleChange = (e) => {
     console.log(e.target.files);
@@ -18,16 +20,26 @@ export default function NewProduct() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    uploadImage(file).then((url) => {
-      console.log(url);
-      addNewProduct(product, url);
-    });
+    setIsUploading(true);
+    uploadImage(file) //
+      .then((url) => {
+        addNewProduct(product, url) //
+          .then(() => {
+            setSuccess("성공적으로 제품이 추가되었습니다.");
+            setTimeout(() => {
+              setSuccess(null);
+            }, 4000);
+          });
+      }) //
+      .finally(() => setIsUploading(false));
     // 제품의 사진을 Cloudinary에 업로드 하고 URL을 획득
     // Firebase에 새로운 제품을 추가함
   };
 
   return (
     <section>
+      <h2>새로운 제품 등록</h2>
+      {success && <p>✅{success}</p>}
       {file && <img src={URL.createObjectURL(file)} alt="local file" />}
       <form onSubmit={handleSubmit}>
         <input
@@ -77,7 +89,10 @@ export default function NewProduct() {
           required
           onChange={handleChange}
         ></input>
-        <Button text={"제품 등록하기"}></Button>
+        <Button
+          text={isUploading ? "업로드중..." : "제품 등록하기"}
+          disabled={isUploading}
+        />
       </form>
     </section>
   );
