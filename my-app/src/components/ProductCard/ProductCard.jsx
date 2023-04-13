4718f920d6987bd8416f094e76911f8d81e3ac5f
khv2644511 <khv2644511@naver.com>
2023-04-13 21:32:11 +0900

    feat: productCard 선택시 ProductDetail 페이지 이동 기능 추가

--- my-app/src/components/ProductCard/ProductCard.jsx
+++ my-app/src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./ProductCard.module.css";
+import { useNavigate } from "react-router-dom";
+
 export default function ProductCard({
+  product,
   product: { id, image, title, category, price },
 }) {
+  const navigate = useNavigate();
   return (
-    <li className={styles.li}>
+    <li
+      className={styles.li}
+      onClick={() => {
+        navigate(`/products/${id}`, { state: { product } });
+      }}
+    >
       <img className={styles.img} src={image} alt={title} />
       <div className={styles.div}>
         <h3 className={styles.h3}>{title}</h3>
