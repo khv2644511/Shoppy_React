e287c5f36a6ff322e9ab259c38ad47251c875b3e
khv2644511 <khv2644511@naver.com>
2023-04-13 17:18:29 +0900

    feat: QueryClientProvider 작성

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { AuthContextProvider } from "./components/context/AuthContext";
 import Navbar from "./components/Navbar/Navbar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <>
-      <AuthContextProvider>
-        <Navbar />
-        <Outlet />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Navbar />
+          <Outlet />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
