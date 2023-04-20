5e699293e089b909698a274a82ca0357e26d3293
khv2644511 <khv2644511@naver.com>
2023-04-20 22:03:26 +0900

    feat: 장바구니 추가 버튼 클릭 시 Database에 선택한 제품 등록 기능

--- my-app/src/pages/ProductDetail/ProductDetail.jsx
+++ my-app/src/pages/ProductDetail/ProductDetail.jsx
@@ -3,9 +3,11 @@ import { useLocation } from "react-router-dom";
 import Button from "../../components/ui/Button";
 import styles from "./ProductDetail.module.css";
 import { useAuthcontext } from "../../context/AuthContext";
+import { addOrUpdateToCart } from "../../api/firebase";
 
 export default function ProductDetail() {
-  const { user } = useAuthcontext();
+  const { uid } = useAuthcontext();
+  console.log(uid);
   const {
     state: {
       product: { id, image, title, category, price, description, options },
@@ -13,7 +15,11 @@ export default function ProductDetail() {
   } = useLocation();
   const [selected, setSelected] = useState(options && options[0]);
   const handleSelect = (e) => setSelected(e.target.value);
-  const handleClick = (e) => {};
+  const handleClick = (e) => {
+    // 장바구니 추가
+    const product = { id, image, title, price, option: selected, quantity: 1 };
+    addOrUpdateToCart(uid, product);
+  };
   return (
     <>
       <p className={styles.category}>{category}</p>
